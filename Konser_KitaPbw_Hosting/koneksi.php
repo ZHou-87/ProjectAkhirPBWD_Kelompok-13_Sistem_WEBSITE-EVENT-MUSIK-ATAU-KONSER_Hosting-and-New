6fa1634fbeee6287ab9be14e40bf39e0200e1b7b
Danang Wahyu Asmudi <danangstudy@gmail.com>
2026-06-23 07:41:07 +0700

    Update database password in koneksi.php

--- Konser_KitaPbw_Hosting/koneksi.php
+++ Konser_KitaPbw_Hosting/koneksi.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Jakarta');
 $conn = mysqli_connect(
     "sql105.infinityfree.com",
     "if0_42219952",
-    "kama1537",
+    "rahasia",
     "if0_42219952_konser_kita"
 );
 
